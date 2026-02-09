a2d06b27d904f13bde6425671279a8f62d03e271
devrisler <devris.isler@gmail.com>
2026-02-09 19:53:01 +0000

    Deploying to gh-pages from @ devrisler/devrisler.github.io@5605720dc9c6886adcec84e4f507afe7af263871 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,16 +440,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-i-have-attended-csp-ias-winter-school-on-cryptography-and-machine-learning-in-turin-italy-url-https-ai4i-it-ias-winter-school-2026-faculty",
+          title: 'I have attended CSP-IAS Winter School on Cryptography and Machine Learning in Turin,...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
