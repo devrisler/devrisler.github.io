e2db1785f161aabcf7864f363a12dcdb6ba9bd76
devrisku <34741946+devrisku@users.noreply.github.com>
2026-03-18 15:19:03 +0000

    Deploying to gh-pages from @ devrisler/devrisler.github.io@60ad8c305211fb92434ec3df8324d6c42ea9dc05 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -529,7 +529,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=NPDGEgQAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
